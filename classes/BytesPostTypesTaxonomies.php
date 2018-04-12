f0dfe83b0ead2c1a4ae383f2ebfecd6b51dd7277
pagresham <piercegresham@gmail.com>
2018-04-12 10:38:26 -0400

    included example post and tax array examples

--- classes/BytesPostTypesTaxonomies.php
+++ classes/BytesPostTypesTaxonomies.php
@@ -2,79 +2,74 @@
 
 if( !class_exists( 'BytesPostsTaxonomies' ) ) {
 
+	// Example of format for post_type array
+	// private $format_post_type = array(
+	// 	'animal' => array(
+	//         'labels' => array(
+	//             'singular'    => 'Animal',
+	//             'plural'      => 'Animals', // optional
+	//             'overrides'   => array(),    // optional
+	//             'text_domain' => ''          // optional
+	//         ),
+	//         'args' => array(
+	//             'public'              => true,
+	//             'has_archive'         => true,
+	//             'menu_position'       => 21,
+	//             'menu_icon'           => 'dashicons-portfolio',
+	//             'supports'            => array('title', 'thumbnail'),
+	//             'taxonomies'          => array('resource_type', 'resource_tag'),
+	//             'exclude_from_search' => true
+	//         ),
+	//     ),
+	// );
+	// Examply of format for taxonomy array
+	// private $test_taxonomies = array(
+	// 	'color' => array(
+	// 		'labels'    => array(
+	//             'singular'    => 'Color',
+	//              // optional
+	//             'overrides'   => array(),    // optional
+	//             'text_domain' => ''          // optional
+	//         ),
+	// 		'args'      => array(
+	// 			// 'labels'            => $this->populate_labels('Service', 'Services'),
+	// 			'hierarchical'      => false,
+	// 			'public'            => true,
+	// 			'show_ui'           => true,
+	// 			'show_admin_column' => true,
+	// 			'show_in_nav_menus' => true,
+	// 			'show_tagcloud'     => true,
+	// 			'rewrite'           => array(
+	// 				'slug'            => 'colors'
+	// 			),
+	// 		),
+	// 	),
+	// );
+
+
+
+
 	class BytesPostsTaxonomies {
 		private static $version = '0.1.0';
-		private static $_this;
 
-
-		public static function register_custom_post_types( $post_types ) {
-			if( is_array( $post_types ) ) {
-
-				foreach( $post_types as $post_key => $post_data ) {
-					$args = array();
-					if( !empty( $post_data['args'] ) ) {
-						$args = $post_data['args'];
-						if( !empty($post_data[ 'singular' ]) && !empty($post_data[ 'plural' ]) ) {
-							
-							$singular_name  = $post_data[ 'singular' ];
-							$plural_name    = $post_data[ 'plural' ];	
-							$args['labels'] = self::populate_post_labels( $singular_name, $plural_name );
-							// var_dump($args['labels']);
-							register_post_type( $post_key, $args );
-						}	
-					}
-				}
-			} 
-		}
-
+		
 		public static function register_taxonomies( $taxonomies ) {
-			if( is_array( $taxonomies ) ) {
-
-				foreach ( $taxonomies as $taxonomy_key => $taxonomy_data ) {
-					$args = array();
-					if ( ! empty( $taxonomy_data['args'] ) ) {
-						$args = $taxonomy_data['args'];
-						if( !empty( $taxonomy_data[ 'singular' ] ) && !empty( $taxonomy_data[ 'plural' ] ) ) {
-							
-							$singular_name    = $taxonomy_data[ 'singular' ];
-							$plural_name      = $taxonomy_data[ 'plural' ];	
-							$args[ 'labels' ] = self::populate_tax_labels( $singular_name, $plural_name );
-
-							var_dump($args['labels']);
-							
-							register_taxonomy( $taxonomy_key, $taxonomy_data['post_types'], $args );
-						}
-					}
-				}
-			} 
-		}	
-
-
-		public static function new_register_taxonomies( $taxonomies ) {
-
-		}
-
-
-		public static function new_register_custom_post_types( $post_types ) {
-			if( !is_array( $post_types ) ) {
+			if( !is_array( $taxonomies ) ) {
 				return;
-			} 
-
-			foreach( $post_types as $post_key => $post_data ) {
-				// var_dump($post_data);
+			}
+			foreach ($taxonomies as $tax_key => $tax_data ) {
 				$args   = array();
 				$labels = array();
-				if( empty( $post_data['args'] ) || empty( $post_data['labels'] ) ) {
+				$post_types = array();
+				if( empty( $tax_data['args'] ) || empty( $tax_data['labels'] ) ) {
 					continue;		
 				}
-				$args   = $post_data['args'];
-				$labels = $post_data['labels'];
-
+				$args   = $tax_data['args'];
+				$labels = $tax_data['labels'];
+				$post_types = ( empty( $tax_data['post_types'] ) ) ? $post_types : $tax_data['post_types'];
 				if( empty($labels['singular']) ) {
 					continue;
 				}
-				$args['label'] = $labels['singular'];
-				
 				$labels = shortcode_atts( array(
 					'singular'  => '',
 					'plural'    => $labels['singular'] . 's',
@@ -82,44 +77,43 @@ if( !class_exists( 'BytesPostsTaxonomies' ) ) {
 					'textdomain' => ''
 				), $labels );
 
-
-				$args['labels'] = self::populate_post_labels( $labels['singular'], $labels['plural'], $labels['overrides'], $labels['textdomain'] );
-
+				$args['labels'] = call_user_func_array( array( 'BytesPostsTaxonomies', 'populate_tax_labels' ), array_values( $labels ) );
 				var_dump($args['labels']);
-				
+				register_taxonomy( $tax_key, $tax_data['post_types'], $args );
+			}
+		}
+
+
+
+		public static function register_custom_post_types( $post_types ) {
+			if( !is_array( $post_types ) ) {
+				return;
+			} 
+			foreach( $post_types as $post_key => $post_data ) {
+				$args   = array();
+				$labels = array();
+				if( empty( $post_data['args'] ) || empty( $post_data['labels'] ) ) {
+					continue;		
+				}
+				$args   = $post_data['args'];
+				$labels = $post_data['labels'];
+				if( empty($labels['singular']) ) {
+					continue;
+				}
+				$args['label'] = $labels['singular'];
+				$labels = shortcode_atts( array(
+					'singular'  => '',
+					'plural'    => $labels['singular'] . 's',
+					'overrides' => array(), 
+					'textdomain' => ''
+				), $labels );
+
+				$args['labels'] = call_user_func_array( array('BytesPostsTaxonomies', 'populate_post_labels'), array_values( $labels ) );
+				var_dump($args['labels']);
 				register_post_type( $post_key, $args );		
 			}
 		}
 
-		public static function new_register_custom_post_types( $post_types ) {
-		    if( !is_array( $post_types ) ) {
-		        return;
-		    }
-		    foreach( $post_types as $post_key => $post_data ) {
-		        $args   = array();
-		        $labels = array();
-		        if( empty( $post_data['args'] ) || empty( $post_data[ 'labels' ] ) ) {
-		            continue;
-		        }
-		        $args   = $post_data['args'];
-		        $labels = $post_data['labels'];
-		        if( empty($labels['singular']) ) {
-		            continue;
-		        }
-		        $args['label'] = $labels['singular'];
-		        $labels = shortcode_atts( array(
-		            'singular'    => '',
-		            'plural'      => $labels['singular'] . 's',
-		            'overrides'   => array(),
-		            'text_domain' => ''
-		        ), $labels );
-		        $args['labels'] = call_user_func_array( array( 'MyClassName', 'populate_post_labels' ), array_values( $labels ) );
-		        register_post_type( $post_key, $args );
-		    }
-		}
-
-
-		
 
 		private static function populate_tax_labels( $singular, $plural, $overrides = array(), $text_domain = "" ) {
 			return shortcode_atts( array(
