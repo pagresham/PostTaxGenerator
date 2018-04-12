4b648f8d9de57cb4a22d85c8f58d52543008eaba
Pierce Gresham <piercegresham@Pierces-MacBook-Air.local>
2018-04-12 08:43:14 -0400

    initial commit

---
+++ EmptyPlugin.php
@@ -0,0 +1,175 @@
+<?php if(!defined('ABSPATH')) { die(); } // Include in all php files, to prevent direct execution
+/**
+ * Plugin Name: EmptyPlugin
+ * Description: A Generic Empty Plugin for project testing
+ * Author: Pierce Gresham
+ * Author URI:
+ * Version: 0.1.1
+ * Text Domain: empty-plugin
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ **/
+if( !class_exists('EmptyPlugin') ) {
+	class EmptyPlugin {
+		private static $version = '0.1.0';
+		private static $_this;
+		private $capability = 'manage_options';
+
+
+		private $new_format_post_type = array(
+			'vehicle' => array(
+		        'labels' => array(
+		            'singular'    => 'Vehicle',
+		            'plural'      => 'Vehicles', // optional
+		            'overrides'   => array(),    // optional
+		            'text_domain' => ''          // optional
+		        ),
+		        'args' => array(
+		            'public'              => true,
+		            'has_archive'         => true,
+		            'menu_position'       => 21,
+		            'menu_icon'           => 'dashicons-portfolio',
+		            'supports'            => array('title', 'thumbnail'),
+		            'taxonomies'          => array('resource_type', 'resource_tag'),
+		            'exclude_from_search' => true
+		        ),
+		    ),
+		);
+
+
+		// taken from the PostTaxClass
+		private $test_post_types = array(
+			'vehicle' => array(
+				'args' => array(
+					'label'               => 'Vehicle',
+					// 'labels'              => $this->populate_labels('Resource', 'Resources'),
+					'public'              => true,
+					'has_archive'         => true,
+					'menu_position'       => 21,
+					'menu_icon'           => 'dashicons-portfolio',
+					'supports'            => array('title', 'thumbnail'),
+					'taxonomies'          => array('resource_type', 'resource_tag'),
+					'exclude_from_search' => true
+				),
+				'singular' => 'Vehicle',
+				'plural'   => 'Vehicles',	
+			),
+			'idea' => array(
+				'args' => array(
+					'label'               => 'Idea',
+					// 'labels'              => $this->populate_labels('Resource', 'Resources'),
+					'public'              => true,
+					'has_archive'         => true,
+					'menu_position'       => 21,
+					'menu_icon'           => 'dashicons-portfolio',
+					'supports'            => array('title', 'thumbnail'),
+					'taxonomies'          => array('resource_type', 'resource_tag'),
+					'exclude_from_search' => true
+				),
+				'singular' => 'Idea',
+				'plural'   => 'Ideas',	
+			),
+			'book' => array(
+				'args' => array(
+					'label'               => 'Book',
+					// 'labels'              => $this->populate_labels('Resource', 'Resources'),
+					'public'              => true,
+					'has_archive'         => true,
+					'menu_position'       => 21,
+					'menu_icon'           => 'dashicons-portfolio',
+					'supports'            => array('title', 'thumbnail'),
+					'taxonomies'          => array('resource_type', 'resource_tag'),
+					'exclude_from_search' => true
+				),
+				'singular' => 'Book',
+				'plural'   => 'Books',	
+			),
+		);
+
+		private $test_taxonomies = array(
+				'service' => array(
+					'args' => array(
+						// 'labels'            => $this->populate_labels('Service', 'Services'),
+						'hierarchical'      => false,
+						'public'            => true,
+						'show_ui'           => true,
+						'show_admin_column' => true,
+						'show_in_nav_menus' => true,
+						'show_tagcloud'     => true,
+						'rewrite'           => array(
+							'slug'            => 'services'
+						),
+					),
+					'post_types'          => array( 'post', 'team', 'portfolio' ),
+					'singular'            => 'Service',
+					'plural'              => 'Services',	
+				),
+			);
+
+		
+		public static function Instance() {
+			static $instance = null;
+			if ($instance === null) {
+				$instance = new self();
+			}
+			return $instance;
+		}
+
+
+		private function __construct() {
+			register_activation_hook( __FILE__, array( $this, 'register_activation' ) );
+			register_deactivation_hook( __FILE__, array( $this, 'register_deactivation' ) );
+
+			add_action( 'init', array( $this, 'plugin_init') );
+			add_action( 'admin_menu', array( $this, 'add_test_page') );
+			add_action( 'init', array( $this, 'register_post_types' ) );
+			add_action( 'init', array( $this, 'register_taxonomies' ) );
+
+		}
+
+		public function register_post_types() {
+			BytesPostsTaxonomies::new_register_custom_post_types( $this->test_post_types );
+		}
+		public function register_taxonomies() {
+			// BytesPostsTaxonomies::register_taxonomies( $this->test_taxonomies );
+		}
+
+
+
+
+		/**
+		 * Register and enque resource styles and scripts
+		 * Owl Carousel and Featherlight libraries
+		 */
+		public function admin_enqueues() {
+		}
+		public function register_activation() {
+		}
+
+		public function register_deactivation() {
+		}
+		
+		public function plugin_init() {
+			include_once 'classes/BytesPostTypesTaxonomies.php';
+		}
+
+		// Function to display tool menu addition 
+		public function add_test_page() {
+			var_dump(array(1, 2, 3));
+			add_submenu_page( 'tools.php', __( 'Empty Plugin', 'empty-plugin' ), __( 'Empyty Plugin', 'empty-plugin' ), $this->capability, 'empty-plugin', array( $this, 'render_test_page' ) );
+		}
+
+
+		public function render_test_page() {
+			?>
+			<h2>output</h2>
+			<?php
+// echo BytesPostsTaxonomies::test_func();
+			
+			// print_r( get_terms( 'service' ) );
+		}
+	}
+	// create instance of Class
+	EmptyPlugin::Instance();
+}
+?>
---
+++ classes/BytesPostTypesTaxonomies.php
@@ -0,0 +1,152 @@
+<?php if(!defined('ABSPATH')) { die(); } // Include in all php files, to prevent direct execution
+
+if( !class_exists( 'BytesPostsTaxonomies' ) ) {
+
+	class BytesPostsTaxonomies {
+		private static $version = '0.1.0';
+		private static $_this;
+
+
+		public static function register_custom_post_types( $post_types ) {
+			if( is_array( $post_types ) ) {
+
+				foreach( $post_types as $post_key => $post_data ) {
+					$args = array();
+					if( !empty( $post_data['args'] ) ) {
+						$args = $post_data['args'];
+						if( !empty($post_data[ 'singular' ]) && !empty($post_data[ 'plural' ]) ) {
+							
+							$singular_name  = $post_data[ 'singular' ];
+							$plural_name    = $post_data[ 'plural' ];	
+							$args['labels'] = self::populate_post_labels( $singular_name, $plural_name );
+
+							register_post_type( $post_key, $args );
+						}	
+					}
+				}
+			} 
+		}
+
+		public static function register_taxonomies( $taxonomies ) {
+			if( is_array( $taxonomies ) ) {
+
+				foreach ( $taxonomies as $taxonomy_key => $taxonomy_data ) {
+					$args = array();
+					if ( ! empty( $taxonomy_data['args'] ) ) {
+						$args = $taxonomy_data['args'];
+						if( !empty( $taxonomy_data[ 'singular' ] ) && !empty( $taxonomy_data[ 'plural' ] ) ) {
+							
+							$singular_name    = $taxonomy_data[ 'singular' ];
+							$plural_name      = $taxonomy_data[ 'plural' ];	
+							$args[ 'labels' ] = self::populate_tax_labels( $singular_name, $plural_name );
+
+							var_dump($args['labels']);
+							
+							register_taxonomy( $taxonomy_key, $taxonomy_data['post_types'], $args );
+						}
+					}
+				}
+			} 
+		}	
+
+
+		public static function new_register_taxonomies( $taxonomies ) {
+
+		}
+
+
+		public static function new_register_custom_post_types( $post_types ) {
+			if( !is_array( $post_types ) ) {
+				return;
+			} 
+			foreach( $post_types as $post_key => $post_data ) {
+				$args   = array();
+				$labels = array();
+				if( empty( $post_data['args'] ) || empty( $post_data['labels'] ) ) {
+					continue;		
+				}
+				$args   = $post_data['args'];
+				$labels = $post_data['labels'];
+
+				if( empty($labels['singular']) ) {
+					continue;
+				}
+				$args['label'] = $labels['singular'];
+				
+				$labels = shortcode_atts( array(
+					'singular'  => '',
+					'plural'    => $labels['singular'] . 's',
+					'overrides' => array(), 
+					'textdomain' => ''
+				), $labels );
+
+
+				$args['labels'] = self::populate_post_labels( $singular, $plural, $overrides, $textdomains );
+				// var_dump($labels);
+				
+				register_post_type( $post_key, $args );		
+			}
+		}
+
+
+		
+
+		private static function populate_tax_labels( $singular, $plural, $overrides = array(), $text_domain = "" ) {
+			return shortcode_atts( array(
+				'name'                       => _x( $plural, 'Taxonomy General Name', $text_domain ),
+				'singular_name'              => _x( $singular, 'Taxonomy Singular Name', $text_domain ),
+				'menu_name'                  => __( $plural, $text_domain ),
+				'all_items'                  => __( 'All '. $plural, $text_domain ),
+				'parent_item'                => __( 'Parent ' . $singular, $text_domain ),
+				'parent_item_colon'          => __( 'Parent ' . $singular . ':', $text_domain ),
+				'new_item_name'              => __( 'New ' . $singular . ' Name', $text_domain ),
+				'add_new_item'               => __( 'Add New ' . $singular, $text_domain ),
+				'edit_item'                  => __( 'Edit ' . $singular, $text_domain ),
+				'update_item'                => __( 'Update ' . $singular, $text_domain ),
+				'view_item'                  => __( 'View ' . $singular, $text_domain ),
+				'separate_items_with_commas' => __( 'Separate ' . $plural . ' with commas', $text_domain ),
+				'add_or_remove_items'        => __( 'Add or remove ' . $plural, $text_domain ),
+				'choose_from_most_used'      => __( 'Choose from the most used', $text_domain ),
+				'popular_items'              => __( 'Popular ' . $plural, $text_domain ),
+				'search_items'               => __( 'Search ' . $plural, $text_domain ),
+				'not_found'                  => __( 'Not Found', $text_domain ),
+				'no_terms'                   => __( 'No ' . $plural, $text_domain ),
+				'items_list'                 => __( $plural . ' list', $text_domain ),
+				'items_list_navigation'      => __( $plural . ' list navigation', $text_domain ),
+			), $overrides );
+		}
+
+
+		private static function populate_post_labels( $singular, $plural, $overrides = array(), $text_domain = "" ) {
+			return shortcode_atts( array(
+				'name'                  => _x( $plural, 'Post Type General Name', $text_domain ),
+				'singular_name'         => _x( $singular, 'Post Type Singular Name', $text_domain ),
+				'menu_name'             => __( $plural, $text_domain ),
+				'name_admin_bar'        => __( $singular, $text_domain ),
+				'archives'              => __( $singular . ' Archives', $text_domain ),
+				'attributes'            => __( $singular . ' Attributes', $text_domain ),
+				'parent_item_colon'     => __( 'Parent ' . $singular . ':', $text_domain ),
+				'all_items'             => __( 'All ' . $plural, $text_domain ),
+				'add_new_item'          => __( 'Add New ' . $singular, $text_domain ),
+				'add_new'               => __( 'Add New', $text_domain ),
+				'new_item'              => __( 'New ' . $singular, $text_domain ),
+				'edit_item'             => __( 'Edit ' . $singular, $text_domain ),
+				'update_item'           => __( 'Update ' . $singular, $text_domain ),
+				'view_item'             => __( 'View ' . $singular, $text_domain ),
+				'view_items'            => __( 'View ' . $plural, $text_domain ),
+				'search_items'          => __( 'Search ' . $plural, $text_domain ),
+				'not_found'             => __( 'Not found', $text_domain ),
+				'not_found_in_trash'    => __( 'Not found in Trash', $text_domain ),
+				'featured_image'        => __( 'Featured Image', $text_domain ),
+				'set_featured_image'    => __( 'Set featured image', $text_domain ),
+				'remove_featured_image' => __( 'Remove featured image', $text_domain ),
+				'use_featured_image'    => __( 'Use as featured image', $text_domain ),
+				'insert_into_item'      => __( 'Insert into ' . $singular, $text_domain ),
+				'uploaded_to_this_item' => __( 'Uploaded to this ' . $singular, $text_domain ),
+				'items_list'            => __( $plural . ' list', $text_domain ),
+				'items_list_navigation' => __( $plural . ' list navigation', $text_domain ),
+				'filter_items_list'     => __( 'Filter ' . $singular . ' list', $text_domain ),
+			) , $overrides );
+		}
+	} 
+} 
